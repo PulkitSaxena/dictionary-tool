4b574b0c9a550dd2da6eca9b0e6a4397309d9f02
pulkit <pulkit.saxena93@gmail.com>
2016-10-10 00:49:59 +0530

    Initial User Input Interface

---
+++ utils/message.js
@@ -0,0 +1,10 @@
+/**
+ * Contains all the messages to be used in app
+ */
+
+ var Message = {
+
+   STARTUP_MESSAGE: 'Dictionary Tool, help command: ./dic --help '
+ };
+
+ module.exports = Message;
